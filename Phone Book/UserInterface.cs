58eba1509a42c98a1f8bd62d30bcc71420d32ead
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-01-25 23:53:04 -0500

    Finished basic userinterface

--- Phone Book/UserInterface.cs
+++ Phone Book/UserInterface.cs
@@ -10,7 +10,39 @@ public static class UserInterface
         while (isAppRunning)
         {
             var option = AnsiConsole.Prompt(
-                new SelectionPrompt<>())
+                new SelectionPrompt<Enums.MainMenuOptions>()
+                    .Title("Phone Book Main Menu")
+                    .AddChoices(
+                        Enums.MainMenuOptions.AddContacts,
+                        Enums.MainMenuOptions.ViewContacts,
+                        Enums.MainMenuOptions.SearchContacts,
+                        Enums.MainMenuOptions.UpdateContacts,
+                        Enums.MainMenuOptions.DeleteContacts,
+                        Enums.MainMenuOptions.Quit
+                    ));
+
+            switch (option)
+            {
+                case Enums.MainMenuOptions.AddContacts:
+                    break;
+                
+                case Enums.MainMenuOptions.ViewContacts:
+                    break;
+                
+                case Enums.MainMenuOptions.SearchContacts:
+                    break;
+                
+                case Enums.MainMenuOptions.UpdateContacts:
+                    break;
+                
+                case Enums.MainMenuOptions.DeleteContacts:
+                    break;
+                
+                case Enums.MainMenuOptions.Quit:
+                    Console.WriteLine("Goodbye!");
+                    isAppRunning = false;
+                    break;
+            }
         }
     }
 }
