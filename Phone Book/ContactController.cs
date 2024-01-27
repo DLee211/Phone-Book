a8ec5c2ab329c16a8cdff370f48ab1cda04d8b47
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-01-27 01:11:59 -0500

    Add Contact funtion

---
+++ Phone Book/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Phone_Book.Models;
+
+namespace Phone_Book;
+
+public class ContactController
+{
+    public static void AddContact(Contact contact)
+    {
+        using var db = new ContactContext();
+        db.Add(contact);
+        db.SaveChanges();
+    }
+}
---
+++ Phone Book/ContactService.cs
@@ -0,0 +1,17 @@
+﻿using Phone_Book.Models;
+using Spectre.Console;
+
+namespace Phone_Book;
+
+public class ContactService
+{
+    public static void InsertContact()
+    {
+        var contact = new Contact();
+
+        contact.Name = AnsiConsole.Ask<string>("Contact Name:");
+        contact.Email = AnsiConsole.Ask<string>("Contact Email:");
+        contact.PhoneNumber = AnsiConsole.Ask<string>("Contact PhoneNumber:");
+        ContactController.AddContact(contact);
+    }
+}
